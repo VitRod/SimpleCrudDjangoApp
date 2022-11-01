b568dc659e236a3130d27628d23a950b4bb0ba41
Vitaliy Rodyna <ukrdao@gmail.com>
2022-11-01 19:58:28 +0200

    templates

--- SimpleCrudDjangoApp/settings.py
+++ SimpleCrudDjangoApp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'members.apps.MembersConfig',
+    'members.apps.MembersConfig'
 ]
 
 MIDDLEWARE = [
