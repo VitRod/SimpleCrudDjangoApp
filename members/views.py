f5d203376e7e36d0f500bb6d89929521e7ef8d9c
Vitaliy Rodyna <ukrdao@gmail.com>
2022-11-01 19:45:08 +0200

    views controller

--- members/views.py
+++ members/views.py
@@ -1,6 +1,6 @@
-
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello world!")
+  template = loader.get_template('myfirst.html')
+  return HttpResponse(template.render())
